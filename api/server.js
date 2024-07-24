989683e99309fd965554dca27a9c270a4a155b3c
DerekG29 <derekgrubbs29@gmail.com>
2024-07-23 20:43:30 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
-// build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+
+server.use(express.json());
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
+
+server.get('*', (req, res, next) => {
+  res.status(400).json({ message: 'Nothing to see here...' });
+})
+
+module.exports = server;
